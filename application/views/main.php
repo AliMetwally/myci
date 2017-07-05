a7d747a42632e8071c4672541a07c2a15f61b874
Ali Elsayed <unique.business.sw@gmail.com>
2017-07-05 13:41:18 +0200

    Create main template
    * main.php view
    ----------------
    create a template view that contains almost required css and js files
    ** before loading the files check if view run locally or from host
    server
    Loaded Files
    css files
    ------
    - bootstrap.min.css
    - bootstrap-theme.min.css
    - font-awesome.min.css
    - font-awesome-animation.min.css
    - main.css
    
    js files
    ---------
    - jquery.min.js
    - bootstrap.min.js
    - main.js

---
+++ application/views/main.php
@@ -0,0 +1,49 @@
+<?php
+defined('BASEPATH') OR exit('');
+$pageTitle = "";
+?>
+
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title><?= $pageTitle ? $pageTitle:$this->uri->segment(1) ?></title>
+        <!--Favicon-->
+        <link rel="shortcut icon"  href=""> <!--add the .ico file-->
+
+        <!--Load Files-->
+        
+        <?php if($_SERVER['HTTP_HOST'] == "localhost" || 
+        stristr($_SERVER['HTTP_HOST'], "192.168.") !== FALSE ||
+        stristr($_SERVER['HTTP_HOST'], "127.0.0.") !== FALSE
+        ): ?>
+
+        <link rel="stylesheet" href="<?= base_url()?>public/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?= base_url()?>public/bootstrap/css/bootstrap-theme.css">
+        <link rel="stylesheet" href="<?= base_url()?>public/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="<?= base_url()?>public/font-awesome/css/font-awesome-animation.css">
+
+        <script src="<?= base_url()?>public/js/jquery.min.js"></script>
+        <script src="<?= base_url()?>public/bootstrap/js/bootstrap.min.js"></script>
+
+        <?php else: ?>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome-animation/0.0.10/font-awesome-animation.min.css">
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+        <?php endif; ?>
+        
+        <!--Custom css-->
+        <link rel="stylesheet" href="<?= base_url()?>public/css/main.css">
+
+        <!--Custome js-->
+        <script src="<?= base_url()?>public/js/main.js"></script>
+    </head>
+    <body>
+        
+    </body>
+</html>
