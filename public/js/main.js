ebaa16a3e25fe82fba5010add063c8e69e50f069
Ali Elsayed <unique.business.sw@gmail.com>
2017-07-16 14:59:08 +0200

    add checkvisibilty function

--- public/js/main.js
+++ public/js/main.js
@@ -144,3 +144,40 @@ function changeFlashMsgContent(msg, iconClassName, color, time){
         setTimeout(function(){$("#flashMsgModal").modal('hide');}, time);
     }
 }
+
+/**
+ * call function "functionToCall" if document has focus
+ * Check Andy E's answer: https://stackoverflow.com/questions/1060008/is-there-a-way-to-detect-if-a-browser-window-is-not-currently-active
+ * @param {type} functionToCall
+ * @returns {undefined}
+ */
+function checkDocumentVisibility(functionToCall){
+    var hidden = "hidden";
+    
+    //detect if page has focus and check login status if it does
+    if(hidden in document){//for browsers that support visibility API
+        $(document).on("visibilitychange", functionToCall);
+    }
+    
+    else if ((hidden = "mozHidden") in document){
+        document.addEventListener("mozvisibilitychange", functionToCall);
+    }
+
+    else if ((hidden = "webkitHidden") in document){
+        document.addEventListener("webkitvisibilitychange", functionToCall);
+    }
+
+    else if ((hidden = "msHidden") in document){
+        document.addEventListener("msvisibilitychange", functionToCall);
+    }
+
+    // IE 9 and lower:
+    else if ("onfocusout" in document){
+        document.onfocusin = document.onfocusout = functionToCall;
+    }
+
+    // All others:
+    else{
+      window.onpageshow = window.onpagehide = window.onfocus = window.onblur = functionToCall;
+    }
+}
