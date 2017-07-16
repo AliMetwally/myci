f50270915fd00141a4d56ad38cba67bddd47c710
Ali Elsayed <unique.business.sw@gmail.com>
2017-07-16 15:01:35 +0200

    add check_session_status

--- application/controllers/Misc.php
+++ application/controllers/Misc.php
@@ -2,5 +2,22 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Misc extends CI_Controller{
-
+    /**
+     * check if admin's session is still on
+     */
+    public function check_session_status(){
+        if(isset($_SESSION['admin_id']) && ($_SESSION['admin_id'] !== false) && ($_SESSION['admin_id'] !== "")){
+            $json['status'] = 1;
+            
+            //update user's last seen time
+            //update_last_seen_time($id, $table_name)
+            $this->genmod->update_last_seen_time($_SESSION['admin_id'], 'admin');
+        }
+        
+        else{
+            $json['status'] = 0;
+        }
+        
+        $this->output->set_content_type('application/json')->set_output(json_encode($json));
+    }
 }
